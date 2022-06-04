7afe20760d91a0fbfb4b8667929b0ddff81b7284
jamesizonfade <izonfadejames@gmail.com>
2022-06-04 17:17:48 +0100

    addded a message for unsupporting pdf browsers

--- pages/millennium-court/index.tsx
+++ pages/millennium-court/index.tsx
@@ -10,7 +10,9 @@ const MillenniumCourt = () =>{
          <Banner page="Millennium-Court" />
          <div className="section-block section-sm border-bottom partners">
           <div className="container">
-              <object data="/documents/millennium-court-bronchure.pdf" type="application/pdf" width="100%" height="2100px"></object>
+              <object data="/documents/millennium-court-bronchure.pdf" type="application/pdf" width="100%" height="2100px"> <p>Oops!! Your browser doesnt support a PDFs</p> 
+              <p><a href="/documents/millennium-court-bronchure.pdf" download="millennium court brochure">Download Millennium Court Brochure</a></p>
+              </object>
           {/* <embed src="/documents/millennium-court-bronchure.pdf" width="100%" height="2100px" /> */}
 
           <div className="millenium-court-button-container text-center">
