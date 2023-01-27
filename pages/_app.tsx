be88171ab17a8f9fecf587320e8adf059ea6ea51
Izonfade James <izonfadejames@gmail.com>
2023-01-27 23:39:23 +0100

    update gallery

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,12 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        
         <meta charSet="UTF-8" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1"
-        />
+        />   
       </Head>
       <Component {...pageProps} />
     </>
